9f15f422abac0d4d5bf623dc0e6883ed2e09a070
Juvenal Manikan <jmanikan@gmail.com>
2015-07-30 02:34:48 -0400

    Added paperclip for img upload; having issues w/ ImageMagick

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "400x600#" } #, :default_url => "/images/:style/missing.png" # Pound sign to crop image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,5 @@ Rails.application.configure do
   # Required for Devise
   # Setup manually
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #Paperclip.options[:command_path] = "/usr/local/bin/identify" <-- Suggested by Stackoverflow
 end
